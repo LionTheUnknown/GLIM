b0b2c778af8d0d650f84dda4076862280f271a69
LionTheUnknown <liutauras.selys@gmail.com>
2025-11-28 03:07:25 +0200

    Fix background position

--- frontend-app/components/PlasmaBackground.tsx
+++ frontend-app/components/PlasmaBackground.tsx
@@ -7,12 +7,8 @@ export default function PlasmaBackground() {
     <div
       style={{
         position: 'fixed',
-        top: 0,
-        left: 0,
         width: '100%',
         height: '100%',
-        zIndex: 0,
-        pointerEvents: 'none',
       }}
     >
       <Plasma
